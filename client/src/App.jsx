6d40ab6dd51811d2d59d53ca8781d3307a65c229
Thiago <teyextreme@outlook.com>
2023-08-09 14:58:58 -0300

    config react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
-function App() {
 
+import * as React from "react";
+
+// Layout
+import { Outlet } from 'react-router-dom'
+
+function App() {
   return (
     <>
-
+      App
+      <Outlet />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "./routes/Home";
+import Login from "./routes/Login.jsx";
+import App from "./App.jsx";
+import ErrorPage from "./routes/ErrorPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children:[
+      {
+        path: '/',
+        element: <Home />
+      }
+    ]
+  },
+  {
+    path: "login",
+    element: <Login />
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
